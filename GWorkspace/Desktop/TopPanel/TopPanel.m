59acd39f0132c52d7c4d9bbea08f75d99867a822
pkgdemon <pkgdemonteam@gmail.com>
2025-06-05 11:38:32 -0500

    Subscribe to menu changes which will be sent by theme

--- GWorkspace/Desktop/TopPanel/TopPanel.m
+++ GWorkspace/Desktop/TopPanel/TopPanel.m
@@ -9,6 +9,12 @@
         _manager = manager;
         [self setAutoresizingMask:NSViewWidthSizable];
         [self setNeedsDisplay:YES];
+
+        // Listen for global menu changes
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(menuChanged:)
+                                                     name:@"NgSTEPGlobalMenuChanged"
+                                                   object:nil];
     }
     return self;
 }
@@ -23,6 +29,34 @@
     CGFloat height = 30.0;
     [self setFrame:NSMakeRect(0, screen.size.height - height, screen.size.width, height)];
     [self setNeedsDisplay:YES];
+}
+
+// Render incoming menu items
+- (void)menuChanged:(NSNotification *)note {
+    NSMenu *menu = note.object;
+
+    // Clear previous subviews
+    for (NSView *v in [self subviews]) {
+        [v removeFromSuperview];
+    }
+
+    // Render each top-level menu item as a button
+    CGFloat x = 10;
+    for (NSMenuItem *item in [menu itemArray]) {
+        NSButton *button = [[NSButton alloc] initWithFrame:NSMakeRect(x, 5, 80, 20)];
+        [button setTitle:[item title]];
+        [button setBezelStyle:NSRoundedBezelStyle];
+        [button sizeToFit];
+        [button setTarget:item.target];
+        [button setAction:item.action];
+        [self addSubview:button];
+        x += button.frame.size.width + 8;
+    }
+}
+
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    [super dealloc];
 }
 
 @end
